b3b10b9ba499ca44a21b4cbb64d0e371d1ddfde5
mushtariy321 <mushtariyrixsiboyev5a@gmail.com>
2024-07-28 19:19:05 +0500

    ozgartirildi

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react"
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
 
   return (
     <>
@@ -16,3 +12,4 @@ function App() {
 }
 
 export default App
+п
